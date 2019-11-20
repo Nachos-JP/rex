95589c010966077deefa8c8dc944b13a50931e59
Shota Maekawa <f22raptor0817@gmail.com>
2019-11-20 21:11:13 +0900

    Add handsontable sample

--- src/views/Parameter.vue
+++ src/views/Parameter.vue
@@ -1,9 +1,21 @@
 <template>
-  <span>Parameter</span>
+  <hot-table :settings="hotSettings"></hot-table>
 </template>
 
 <script>
-export default {
+import {HotTable} from "@handsontable/vue";
+import Handsontable from "handsontable";
 
+export default {
+  data: () => ({
+    hotSettings: {
+      data: Handsontable.helper.createSpreadsheetData(6, 10),
+      colHeaders: true,
+      licenseKey: "non-commercial-and-evaluation",
+    },
+  }),
+  components: {
+    HotTable,
+  },
 };
 </script>
