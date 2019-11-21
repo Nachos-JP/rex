b03339cfce34bc1b4fff590a203ef7c048a76f10
Shota Maekawa <f22raptor0817@gmail.com>
2019-11-21 22:54:14 +0900

    Use v-container in parameter

--- src/views/Parameter.vue
+++ src/views/Parameter.vue
@@ -1,5 +1,32 @@
 <template>
-  <hot-table :settings="hotSettings"></hot-table>
+  <v-container fluid fill-height>
+    <v-row class="ml-5" style="height:50%;">
+      <v-container>
+        <v-row>
+          <v-chip color="primary" outlined>
+            <v-icon left>input</v-icon>
+            Input
+          </v-chip>
+        </v-row>
+        <v-row class="mt-2">
+          <hot-table :settings="hotSettings"></hot-table>
+        </v-row>
+      </v-container>
+    </v-row>
+    <v-row class="ml-5" style="height:50%;">
+      <v-container>
+        <v-row>
+          <v-chip color="primary" outlined>
+            <v-icon left>assessment</v-icon>
+            Output
+          </v-chip>
+        </v-row>
+        <v-row class="mt-2">
+          <hot-table :settings="hotSettings"></hot-table>
+        </v-row>
+      </v-container>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
